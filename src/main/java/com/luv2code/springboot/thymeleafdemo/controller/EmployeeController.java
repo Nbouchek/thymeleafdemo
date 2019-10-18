07ea96f3adea1477021feec2fe26e52d3e01479d
Nacer Bouchekhima <nbouchek@gmail.com>
2019-10-18 11:20:32 +0100

    Add dao and service classes

--- src/main/java/com/luv2code/springboot/thymeleafdemo/controller/EmployeeController.java
+++ src/main/java/com/luv2code/springboot/thymeleafdemo/controller/EmployeeController.java
@@ -1,7 +1,6 @@
 package com.luv2code.springboot.thymeleafdemo.controller;
 
-
-import com.luv2code.springboot.thymeleafdemo.model.Employee;
+import com.luv2code.springboot.thymeleafdemo.entity.Employee;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
