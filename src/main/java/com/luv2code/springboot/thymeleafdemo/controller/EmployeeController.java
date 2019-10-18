ffec6b789fec435dc6a22e283976a7028fa489ee
Nacer Bouchekhima <nbouchek@gmail.com>
2019-10-18 12:48:50 +0100

    Show the add employee formbutton

--- src/main/java/com/luv2code/springboot/thymeleafdemo/controller/EmployeeController.java
+++ src/main/java/com/luv2code/springboot/thymeleafdemo/controller/EmployeeController.java
@@ -27,4 +27,12 @@ public class EmployeeController {
 
         return "employees/list-employees";
     }
+
+    @GetMapping("/showFormForAdd")
+    public String showFormForAdd(Model theModel) {
+        // Create model attribute to bind form data
+        Employee theEmployee = new Employee();
+        theModel.addAttribute("employee", theEmployee);
+        return "employees/employee-form";
+    }
 }
